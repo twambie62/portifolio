577e430d0c624f408260e0b9f1b781be2428dd0f
ÞttmfxÞ <twambililemusukwa@gmail.com>
2026-03-24 03:49:19 +0200

    made final touches

--- assets/app_shared.js
+++ assets/app_shared.js
@@ -4,7 +4,7 @@ function renderHeader() {
         <div class="container">
             <nav class="app-nav">
                 <div class="logo"><span>t.</span></div>
-                <a href="../portifolio.html" class="active">Home</a>
+                <a href="../index.html" class="active">Home</a>
                 <a href="#about">About</a>
                 <a href="#projects">Projects</a>
                 <a href="#contact">Contact</a>
@@ -12,16 +12,16 @@ function renderHeader() {
         </div>
     `;
 }
-function renderFooter() {
-    document.getElementById('app-footer').innerHTML = `
-        <footer class="socials">
-            <a class="icon-btn" href="#"><i class="ri-linkedin-fill"></i></a>
-            <a class="icon-btn" href="#"><i class="ri-facebook-circle-fill"></i></a>
-            <a class="icon-btn" href="#"><i class="ri-instagram-line"></i></a>
-            <a class="icon-btn" href="#"><i class="ri-twitter-x-line"></i></a>
-        </footer>
-    `;
-}
+// function renderFooter() {
+//     document.getElementById('app-footer').innerHTML = `
+//         <footer class="socials">
+//             <a class="icon-btn" href="#"><i class="ri-linkedin-fill"></i></a>
+//             <a class="icon-btn" href="#"><i class="ri-facebook-circle-fill"></i></a>
+//             <a class="icon-btn" href="#"><i class="ri-instagram-line"></i></a>
+//             <a class="icon-btn" href="#"><i class="ri-twitter-x-line"></i></a>
+//         </footer>
+//     `;
+// }
 renderHeader();
-renderFooter();
+// renderFooter();
 
