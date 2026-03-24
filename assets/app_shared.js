2113fec5c51a60b6e390f2688e9385f74511ff53
ÞttmfxÞ <twambililemusukwa@gmail.com>
2026-03-25 01:10:09 +0200

    fixed routes

--- assets/app_shared.js
+++ assets/app_shared.js
@@ -5,23 +5,52 @@ function renderHeader() {
             <nav class="app-nav">
                 <div class="logo"><span>t.</span></div>
                 <a href="../index.html" class="active">Home</a>
-                <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
+                <a href="../index.html#about">About Me</a>
+                <a href="../index.html#projects">Projects</a>
+                <a href="../index.html#contact">Contact</a>
             </nav>
         </div>
     `;
 }
-// function renderFooter() {
-//     document.getElementById('app-footer').innerHTML = `
-//         <footer class="socials">
-//             <a class="icon-btn" href="#"><i class="ri-linkedin-fill"></i></a>
-//             <a class="icon-btn" href="#"><i class="ri-facebook-circle-fill"></i></a>
-//             <a class="icon-btn" href="#"><i class="ri-instagram-line"></i></a>
-//             <a class="icon-btn" href="#"><i class="ri-twitter-x-line"></i></a>
-//         </footer>
-//     `;
-// }
+function renderFooter() {
+    document.getElementById('app-footer').innerHTML = `
+<head>
+
+</head>
+  <div class="footer-container">
+    <div class="footer-top">
+      <div class="footer-left">
+        <div class="footer-title">Do You have<br>any questions?</div>
+        <div class="footer-sub">Feel free to email me</div>
+        <div class="footer-email">twambililemusukwa@gmail.com</div>
+        <div class="footer-socials">
+          <a href="#" aria-label="LinkedIn" style="width:28px; height: 28px" class="footer-social"><img src="../assets/images/footer/linkedin.svg" alt="LinkedIn" /></a>
+          <a href="#" aria-label="Facebook" style="width:25px; height: 25px" class="footer-social"><img src="../assets/images/footer/facebook.svg" alt="Facebook" /></a>
+          <a href="#" aria-label="Instagram" style="width:25px; height: 25px" class="footer-social"><img src="../assets/images/footer/instagram.svg" alt="Instagram" /></a>
+          <a href="#" aria-label="X" style="width:21px; height: 21px; margin-top: 2px" class="footer-social"><img src="../assets/images/footer/twitter_x.svg" alt="X" /></a>
+        </div>
+      </div>
+      <div class="footer-right">
+        <div class="footer-contact-title">Contact me</div>
+        <div class="footer-contact-phone">+265 99-588-64-98</div>
+        <div class="footer-contact-phone">+265 88-485-08-93</div>
+        <div class="footer-location-title">Location</div>
+        <div class="footer-location">Lilongwe,Malawi</div>
+      </div>
+    </div>
+    <div class="footer-divider"></div>
+    <div class="footer-copyright">© 2026 Twambi. All rights reserved</div>
+    <div class="footer-svg-bg">
+      <img src="../assets/images/footer/twambi_footer.svg" alt="Twambi" />
+      <span class="footer-dot"></span>
+    </div>
+    <div class="footer-bottom-bar">
+      Made with <span class="footer-heart">♥</span> by <span class="footer-author">Twambi Musukwa</span> Thank you <span class="footer-scroll">For scrolling</span>
+    </div>
+  </div>
+
+    `;
+}
 renderHeader();
-// renderFooter();
+renderFooter();
 
